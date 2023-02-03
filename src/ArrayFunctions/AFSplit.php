c630e968182312c653e1290ad8807152fb1ad36e
Marijn van Wezel <marijnvanwezel@gmail.com>
2023-02-03 16:00:40 +0100

    Add #af_split function
    Change-Id: I376039845774b45536e75cb325691c56130301e8

---
+++ src/ArrayFunctions/AFSplit.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ArrayFunctions\ArrayFunctions;
+
+use ArrayFunctions\Utils;
+
+/**
+ * Implements the #af_split parser function.
+ */
+class AFSplit extends ArrayFunction {
+	/**
+	 * @inheritDoc
+	 */
+	public static function getName(): string {
+		return 'af_split';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function execute( string $string, ?string $separator = ',' ): array {
+		return [ explode( Utils::unescape( $separator ?? ',' ), $string ) ];
+	}
+}
